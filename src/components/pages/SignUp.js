dc27780fece7dd129532ae891bb4eda521207ee2
Colin Masterson <colinmasterson97@hotmail.com>
2022-01-05 20:51:42 +0000

    Signup Page styles

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,35 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SignUp() {
     return (
         <section className='signup flex'>
-            <div className='container'>
+            <div className='container align'>
                 <h2>
                     Sign up to <span>MyGamesBacklog</span>
                 </h2>
-                <form>
-                    <label htmlFor='username'>Username</label>
-                    <input
-                        type='text'
-                        id='username'
-                        name='username'
-                        placeholder='Username'
-                    />
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        type='password'
-                        id='password'
-                        name='password'
-                        placeholder='Password'
-                    />
-                    <label htmlFor='confpassword'>Confirm Password</label>
-                    <input
-                        type='password'
-                        id='confpassword'
-                        name='confpassword'
-                        placeholder='Confirm Password'
-                    />
+                <form className='grid'>
+                    <div className='form-input'>
+                        <label htmlFor='username'>Username:</label>
+                        <input
+                            type='text'
+                            id='username'
+                            name='username'
+                            placeholder='Username'
+                        />
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='email'>Email:</label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='email@example.com'
+                        />
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='password'>Password:</label>
+                        <input
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Password'
+                        />
+                    </div>
+                    <div className='form-input last'>
+                        <label htmlFor='confpassword'>Re-type Password:</label>
+                        <input
+                            type='password'
+                            id='confpassword'
+                            name='confpassword'
+                            placeholder='Re-type Password'
+                        />
+                    </div>
+                    <p>
+                        Already have an account?{' '}
+                        <Link to='/login' className='login'>
+                            Login
+                        </Link>
+                    </p>
                 </form>
+                <input type='button' className='btn' value='Create Account' />
             </div>
         </section>
     );
