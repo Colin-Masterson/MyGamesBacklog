7e14d8ae109ca059569d52c0523392d341424e4b
Colin Masterson <colinmasterson97@hotmail.com>
2022-01-05 15:58:12 +0000

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Landing from './components/Landing';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import Profile from './components/Profile';
-import GamesList from './components/GamesList';
-import AddGame from './components/AddGame';
+import Landing from './components/pages/Landing';
+import SignIn from './components/pages/SignIn';
+import SignUp from './components/pages/SignUp';
+import Profile from './components/pages/Profile';
+import GamesList from './components/pages/GamesList';
+import AddGame from './components/pages/AddGame';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
