a05639c4ebc4e58283162d2e5ebfe40ddeb2b2b8
Colin Masterson <colinmasterson97@hotmail.com>
2022-01-05 19:16:50 +0000

    Signup layout

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
 function SignUp() {
-    return <div>SignUp</div>;
+    return (
+        <section className='signup flex'>
+            <div className='container'>
+                <h2>
+                    Sign up to <span>MyGamesBacklog</span>
+                </h2>
+                <form>
+                    <label htmlFor='username'>Username</label>
+                    <input
+                        type='text'
+                        id='username'
+                        name='username'
+                        placeholder='Username'
+                    />
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        type='password'
+                        id='password'
+                        name='password'
+                        placeholder='Password'
+                    />
+                    <label htmlFor='confpassword'>Confirm Password</label>
+                    <input
+                        type='password'
+                        id='confpassword'
+                        name='confpassword'
+                        placeholder='Confirm Password'
+                    />
+                </form>
+            </div>
+        </section>
+    );
 }
 
 export default SignUp;
