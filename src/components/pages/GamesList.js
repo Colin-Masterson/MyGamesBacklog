c3fd729d916b1d7037ffcdee07d16f9b9a259186
Colin Masterson <colinmasterson97@hotmail.com>
2022-01-26 19:54:52 +0000

    Can now delete game from your list

--- src/components/pages/GamesList.js
+++ src/components/pages/GamesList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import GameCard from '../misc/GameCard';
+
 import Header from '../misc/Header';
 import { getGames, auth } from '../../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -7,6 +7,18 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 function GamesList() {
     const [user, loading] = useAuthState(auth);
     const [games, setGames] = useState([]);
+
+    const deleteGame = async (id) => {
+        try {
+            await fetch(`http://localhost:5000/games/${id}`, {
+                method: 'DELETE',
+            });
+
+            setGames(games.filter((game) => game.game_id !== id));
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
 
     useEffect(() => {
         if (loading) {
@@ -23,7 +35,16 @@ function GamesList() {
             <Header />
 
             {games.map((game) => (
-                <GameCard key={game.id} game={game} />
+                <div key={game.game_id} className='game-card'>
+                    <div className='grid container'>
+                        <h5 className='game-name'>{game.gamename}</h5>
+                        <p className='game-system'>{game.gamesystem}</p>
+                        <p className='game-status'>{game.gamestatus}</p>
+                        <button onClick={() => deleteGame(game.game_id)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
             ))}
         </>
     );
