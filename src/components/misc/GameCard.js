faaed92305d0947c121af84ba857599d4f96e9c0
Colin Masterson <colinmasterson97@hotmail.com>
2022-01-26 20:33:54 +0000

    delete games

--- src/components/misc/GameCard.js
+++ src/components/misc/GameCard.js
@@ -7,6 +7,9 @@ function GameCard(props) {
                 <h5 className='game-name'>{props.game.gamename}</h5>
                 <p className='game-system'>{props.game.gamesystem}</p>
                 <p className='game-status'>{props.game.gamestatus}</p>
+                <button onClick={() => deleteGame(props.game.game_id)}>
+                    Delete
+                </button>
             </div>
         </div>
     );
