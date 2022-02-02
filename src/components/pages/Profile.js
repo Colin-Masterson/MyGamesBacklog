2a8096844d7fa867cbee8cec8f516f221da9bb3b
Colin Masterson <colinmasterson97@hotmail.com>
2022-02-02 21:20:49 +0000

    display game data on profile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,23 +1,85 @@
-import { useEffect } from 'react';
-import Header from '../misc/Header';
-import { auth } from '../../firebase';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { getData, auth } from '../../firebase';
+import Header from '../misc/Header';
 
 function Profile() {
     const [user, loading] = useAuthState(auth);
-
+    const [userdata, setUserData] = useState([]);
     useEffect(() => {
         if (loading) {
-            return;
+            return <h1>Loading...</h1>;
         }
+
+        getData(user).then((data) => {
+            setUserData(data);
+        });
     }, [user, loading]);
 
-    return (
-        <>
-            <Header />
-            <h3>Profile</h3>
-        </>
-    );
+    const { totalgames, inprogress, backlog, finished, completed, playing } =
+        userdata;
+
+    if (playing) {
+        return (
+            <>
+                <Header />
+                <section className='profile'>
+                    <div className='container grid'>
+                        <div className='stats main'>
+                            <h2>Colin's Stats</h2>
+
+                            <div>
+                                <div className='stat'>
+                                    <h4>Total Games:</h4>
+                                    <h5>{totalgames}</h5>
+                                </div>
+                                <div className='stat'>
+                                    <h4>Backlog:</h4>
+                                    <h5>{backlog}</h5>
+                                </div>
+                                <div className='stat'>
+                                    <h4>In Progress:</h4>
+                                    <h5>{inprogress}</h5>
+                                </div>
+                                <div className='stat'>
+                                    <h4>Finished:</h4>
+                                    <h5>{finished}</h5>
+                                </div>
+                                <div className='stat'>
+                                    <h4>Completed:</h4>
+                                    <h5>{completed}</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='stats games'>
+                            <h2>Games In Progress</h2>
+                            <div className='list'>
+                                {playing.map((data) => {
+                                    return (
+                                        <div
+                                            key={data.game_id}
+                                            className='stat'
+                                        >
+                                            <h4>{data.gamename}</h4>
+                                            <h5>{data.gamesystem}</h5>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        {/* <div className='stats systems'>
+                            <h2>Games In Progress</h2>
+                            <div className='list'>
+                                h
+                            </div>
+                        </div> */}
+                    </div>
+                </section>
+            </>
+        );
+    } else {
+        return <h1>Loading....</h1>;
+    }
 }
 
 export default Profile;
